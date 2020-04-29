f892a53201257dadc80e561097c9ab0671cc99c0
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-04-29 12:19:18 +0100

    Packages used in workshop

---
+++ packages.R
@@ -0,0 +1,19 @@
+# Packages used in workshop
+# Install in base package of RStudio Cloud: rstd.io/rit-cloud
+
+install.packages("devtools")
+install.packages("DT")
+install.packages("flexdashboard")
+install.packages("gapminder")
+install.packages("learnr")
+install.packages("lubridate")
+install.packages("infer")
+install.packages("magick")
+install.packages("nycflights13")
+install.packages("rvest")
+install.packages("shiny")
+\install.packages("tidyverse")
+install.packages("unvotes")
+
+devtools::install_github("gadenbuie/countdown")
+devtools::install_github("OpenIntroStat/openintro-r-package")
